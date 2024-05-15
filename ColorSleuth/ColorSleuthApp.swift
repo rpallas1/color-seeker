f13af7d4b6a95b456f50502fece0b29c67542570
Rpallas1 <rpallas1@asu.edu>
2024-05-15 09:58:22 -0700

    Initial Commit

---
+++ ColorSleuth/ColorSleuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSleuthApp.swift
+//  ColorSleuth
+//
+//  Created by Ryan Pallas on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorSleuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

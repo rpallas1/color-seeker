555880162217583f86ee3fbdae220a7dbf64d731
Rpallas1 <rpallas1@asu.edu>
2024-05-28 15:27:26 -0700

    - fix time formatting bug when tenths place is a 0

--- ColorSleuth/Helper/FormatHelper.swift
+++ ColorSleuth/Helper/FormatHelper.swift
@@ -19,7 +19,8 @@ struct FormatHelper {
 		
 		func time(elapsedTime: Double) -> String {
 				if elapsedTime < 60 {
-						if elapsedTime.truncatingRemainder(dividingBy: 1) == 0 {
+						let tenthsPlace = Int((elapsedTime * 10).truncatingRemainder(dividingBy: 10))
+						if tenthsPlace == 0 {
 								return String(format: "%.0fs", elapsedTime)
 						} else {
 								return String(format: "%.1fs", elapsedTime)

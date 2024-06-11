5b4cfe43860b84cc36f2a02c1a0b4e5e4cd8fd0d
Rpallas1 <rpallas1@asu.edu>
2024-06-11 14:29:23 -0700

    - change round to game in end of game summary

--- ColorSleuth/ColorSleuthApp.swift
+++ ColorSleuth/ColorSleuthApp.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import SwiftData
-//import TelemetryDeck
 
 @main
 struct ColorSleuthApp: App {
@@ -15,11 +14,6 @@ struct ColorSleuthApp: App {
 		@State var viewStates = GlobalStates()
 		@State private var settings = Settings()
 		@AppStorage("needsOnboarding") var needsOnboarding: Bool = true
-
-//		init() {
-//				let config = TelemetryDeck.Config(appID: "81C31ED3-4AB5-4765-BE71-9345960BB4DA")
-//				TelemetryDeck.initialize(config: config)
-//		}
 		
     var body: some Scene {
         WindowGroup {
